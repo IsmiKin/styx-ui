38d9881ead7617d06b79bcb7ad766c2d4e9dddbf
Ismael Ors <ismael.ors@ubeeqo.com>
2020-07-26 12:01:06 +0200

    Refactor Dead Code form into new component

---
+++ src/components/DeadCodeReport/ReportForm.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="ReportForm">
+    <section class="mb-8">
+      <h1>Run Report</h1>
+      <div class="subtitle-1">Fill the types of reports you want to run</div>
+    </section>
+    <v-divider></v-divider>
+    <section>
+      <v-form ref="dead-code-report-form">
+        <v-text-field
+          v-for="input in reportFormInputs"
+          v-model="deadCodeReportForm[input.key]"
+          class="my-4"
+          persistent-hint
+          required
+          :key="input.key"
+          :prepend-icon="input.icon"
+          :label="input.label"
+          :hint="input.hint"
+        >
+          <template v-slot:append>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on }">
+                <v-icon v-on="on">mdi-help-circle-outline</v-icon>
+              </template>
+              {{ input.hint }}
+            </v-tooltip>
+          </template>
+        </v-text-field>
+        <v-btn color="success" :loading="isLoading" class="mt-4 mr-4" @click="runReport">
+          <v-icon>mdi-play</v-icon>Run
+        </v-btn>
+      </v-form>
+    </section>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "ReportForm",
+  data() {
+    return {
+      isLoading: false,
+      reportFormInputs: [
+        {
+          key: "projectPath",
+          label: "Project Path",
+          icon: "mdi-folder",
+          hint:
+            "Project where the scanner would run the report. It would search files matching criteria and resolve imports.",
+        },
+        {
+          key: "packageJson",
+          label: "Package json",
+          icon: "mdi-package-variant-closed",
+          hint:
+            "Project package.json path. It would be use to ignore vendor packages on the imports statements.",
+        },
+        {
+          key: "projectOptions",
+          label: "Project Options",
+          icon: "mdi-format-list-checks",
+          hint:
+            "Project options path. Specific options for the scanner (formats, ignores...) MORE INFO COMING.",
+        },
+      ],
+    };
+  },
+  computed: {
+    ...mapGetters("projectReportsStore", ["deadCodeReportForm"]),
+  },
+  methods: {
+    ...mapActions("projectReportsStore", ["generateDeadCodeReport"]),
+
+    async runReport() {
+      this.isLoading = true;
+      await this.generateDeadCodeReport();
+      this.isLoading = false;
+    },
+  },
+};
+</script>
--- src/views/DeadCodeReport.vue
+++ src/views/DeadCodeReport.vue
@@ -48,7 +48,7 @@
 
               <v-chip class="ma-2" :color="section.color">
                 {{
-                deadCodeReport.report.stats[section.statsKey]
+                get(deadCodeReport, `report.stats.${section.statsKey}`)
                 }}
               </v-chip>
             </div>
@@ -66,83 +66,33 @@
         </v-expansion-panel>
       </v-expansion-panels>
     </div>
-    <div v-else>
-      <section class="mb-8">
-        <h1>Run Report</h1>
-        <div class="subtitle-1">Fill the types of reports you want to run</div>
-      </section>
-      <v-divider></v-divider>
-      <section>
-        <v-form ref="dead-code-report-form">
-          <v-text-field
-            v-for="input in reportFormInputs"
-            v-model="deadCodeReportForm[input.key]"
-            class="my-4"
-            persistent-hint
-            required
-            :key="input.key"
-            :prepend-icon="input.icon"
-            :label="input.label"
-            :hint="input.hint"
-          >
-            <template v-slot:append>
-              <v-tooltip bottom>
-                <template v-slot:activator="{ on }">
-                  <v-icon v-on="on">mdi-help-circle-outline</v-icon>
-                </template>
-                {{ input.hint }}
-              </v-tooltip>
-            </template>
-          </v-text-field>
-          <v-btn color="success" :loading="isLoading" class="mt-4 mr-4" @click="runReport">
-            <v-icon>mdi-play</v-icon>Run
-          </v-btn>
-        </v-form>
-      </section>
-    </div>
+    <ReportForm v-else />
   </div>
 </template>
 
 <script>
-import _ from "lodash";
-import { mapGetters, mapActions, mapMutations } from "vuex";
+import { get, startCase, size } from "lodash";
+import { mapGetters, mapMutations, mapActions } from "vuex";
 
+import ReportForm from "@/components/DeadCodeReport/ReportForm";
 import ReportDataTable from "@/components/DeadCodeReport/ReportDataTable";
 import ReportErrorTable from "@/components/DeadCodeReport/ReportErrorTable";
 import ReportIsolatesTable from "@/components/DeadCodeReport/ReportIsolatesTable";
 
 export default {
   name: "DeadCodeReport",
-  components: { ReportDataTable, ReportErrorTable, ReportIsolatesTable },
+  components: {
+    ReportDataTable,
+    ReportErrorTable,
+    ReportIsolatesTable,
+    ReportForm,
+  },
   data() {
     return {
       isLoading: false,
-      reportFormInputs: [
-        {
-          key: "projectPath",
-          label: "Project Path",
-          icon: "mdi-folder",
-          hint:
-            "Project where the scanner would run the report. It would search files matching criteria and resolve imports.",
-        },
-        {
-          key: "packageJson",
-          label: "Package json",
-          icon: "mdi-package-variant-closed",
-          hint:
-            "Project package.json path. It would be use to ignore vendor packages on the imports statements.",
-        },
-        {
-          key: "projectOptions",
-          label: "Project Options",
-          icon: "mdi-format-list-checks",
-          hint:
-            "Project options path. Specific options for the scanner (formats, ignores...) MORE INFO COMING.",
-        },
-      ],
       tableSections: [
         {
-          name: _.startCase("errors"),
+          name: startCase("errors"),
           key: "errors",
           statsKey: "errors",
           color: "red",
@@ -152,7 +102,7 @@ export default {
             "Errors ocurring when scanning the imports from in a file. Mainly issues trying resolving the import path",
         },
         {
-          name: _.startCase("isolates"),
+          name: startCase("isolates"),
           key: "isolates",
           statsKey: "isolates",
           color: "warning",
@@ -161,7 +111,7 @@ export default {
           tooltipInfo: "Files which has not being imported for any other file.",
         },
         {
-          name: _.startCase("report"),
+          name: startCase("report"),
           key: "dataReport",
           statsKey: "scanned_files",
           color: "primary",
@@ -178,17 +128,17 @@ export default {
       "deadCodeReport",
       "deadCodeReportForm",
     ]),
+
     skeletonItemsVisible() {
-      return this.isLoading ? _.size(this.tableSections) : [];
+      return this.isLoading ? size(this.tableSections) : [];
     },
   },
   methods: {
+    get,
     ...mapMutations("projectReportsStore", {
       resetDeadCodeReportData: "RESET_DEAD_CODE_REPORT_DATA",
     }),
-
     ...mapActions("projectReportsStore", ["generateDeadCodeReport"]),
-
     async runReport() {
       this.isLoading = true;
       await this.generateDeadCodeReport();
